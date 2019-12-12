6717ac7fb856a6a9a79c0739ec2ade21bf8901ed
Karam <karamjoudah@hotmail.com>
2019-12-12 15:51:41 -0500

    Added explanation to main function in LogRxOperators

--- LogRxOperators/app/src/main/java/com/ford/logrxoperators/OperatorsPlayground.kt
+++ LogRxOperators/app/src/main/java/com/ford/logrxoperators/OperatorsPlayground.kt
@@ -4,12 +4,26 @@ import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
 
+/*
+
+ Test the behaviour of observables, operators, singles, and so on by using the
+ log function to print the status of an observable at each point of its "lifecycle"
+
+ The log function uses Emoji's in order to make logging easier. You can change the emoji being
+ displayed by passing your Emoji of choice into the log function.
+
+ */
+
+
+
+
+
 fun main() {
 
 
     val obsA = Observable.interval(2, TimeUnit.SECONDS).log(Emoji.completed)
 
-    var obsB = Observable.interval(10, TimeUnit.SECONDS).log(Emoji.b_emoji)
+    val obsB = Observable.interval(10, TimeUnit.SECONDS).log(Emoji.b_emoji)
 
 
     obsA.takeUntil(obsB).subscribe {
